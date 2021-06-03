045b604eea84fae4ef8a7ea58f52296feb15b604
Budgo <Budgo@mail.ru>
2021-06-03 17:32:58 +0300

    Не подключается постмен
    Signed-off-by: Budgo <Budgo@mail.ru>

---
+++ Temperature Controller/Temperature Controller/Controllers/TemperatureControllerCrud.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Temperature_Controller.Controllers
+{
+    [Route("api/crud")]
+    [ApiController]
+    public class TemperatureControllerCrud : ControllerBase
+    {
+        private readonly IValuesHolder _holder;
+        public TemperatureControllerCrud(IValuesHolder holder)
+        {
+            _holder = holder;
+        }
+
+        [HttpPost("create")]
+        public IActionResult Create([FromQuery] string temperatur, [FromQuery] string time)
+        {
+            _holder.Add(temperatur, time);
+            return Ok();
+        }
+
+        [HttpGet("read")]
+        public IActionResult Read([FromQuery] string timeOne, [FromQuery] string timeTwo)
+        {
+            return Ok(_holder.Get(timeOne, timeTwo));
+        }
+
+        [HttpPut("update")]
+        public IActionResult Update([FromQuery] string temperatur, [FromQuery] string time)
+        {
+            _holder.Update(temperatur, time);
+            return Ok();
+        }
+
+        [HttpDelete("delete")]
+        public IActionResult Delete([FromQuery] string timeOne, [FromQuery] string timeTwo)
+        {
+            _holder.Delete(timeOne, timeTwo);
+            return Ok();
+        }
+    }
+}
